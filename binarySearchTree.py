3c64148540f2492ff2a2b1ddf62c32406bc93375
Louis Echefu <louis@Louiss-MacBook-Air.local>
2023-12-02 11:32:53 -0600

    recursion to delete a node

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -60,8 +60,32 @@ class BinarySearchTree:
             current_node.left = self.__r_insert(current_node.left,value)
         if value > current_node.value:
             current_node.right = self.__r_insert(current_node.right,value)
+        return 
+    def min_value(self,current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+    
+    def __delete_node(self,current_node,value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right==None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+                return None
+            elif current_node.right == None:
+                current_node = current_node.left
+                return None
+            else:
+                
         return current_node
-            
+        
     def r_contains(self,value):
         return self.__r_contains(self.root,value)
     
@@ -69,8 +93,10 @@ class BinarySearchTree:
         if self.root is None:
             new_node = Node(value)
             self.root = new_node
-            return True
-        self.__r_contains(self.root,value)
+        self.__r_insert(self.root,value)
+        
+    def delete_node(self,value):
+        self.root = self.__delete_node(self.root,value)
         
     
 
